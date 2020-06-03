51ab9c6c80aa5a8633b295ccc9944ae79ba4aecf
duyht56 <duyht56@gmail.com>
2020-06-03 23:58:55 +0700

    wip

---
+++ app/src/app/antd.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+@NgModule({
+    declarations: [],
+    imports: [
+        NzButtonModule,
+    ],
+    exports: [
+        NzButtonModule
+    ],
+    providers: []
+})
+
+export class AntdModule {
+    constructor() { }
+}
---
+++ app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { AntdModule } from './antd.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+
+    AntdModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
